fd852dc46f354498ea4942aa47e4b48b16dce4a8
vitthalwankhede-tudip <vitthal.wankhede@tudip.nl>
2016-09-17 16:30:01 +0530

    MVC Employee Application

---
+++ MVCEmployee/MVCEmployee/Models/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MVCEmployee.Models
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public DateTime JoiningDate { get; set; }
+        public string Position { get; set; }
+        public decimal Salary { get; set; }
+
+    }
+    public class EmployeeDBContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+    }
+
+}
